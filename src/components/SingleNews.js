9a86c25da5b6649a820acd6055ee40a562ead8a0
Aid Puska <aidpuska29@gmail.com>
2023-08-26 14:32:40 +0200

    added grid system correctly for news home page

--- src/components/SingleNews.js
+++ src/components/SingleNews.js
@@ -4,29 +4,29 @@ import React from 'react'
 import ImageWrapper from './ImageWrapper'
 import Image from 'next/image'
 
-const SingleNews = ({size}) => {
+const SingleNews = ({size, news}) => {
 
   const desc = 'Visoki predstavnik u BiH danas je u Sarajevu održao nastupnu konferenciju za medije, na kojoj je govorio kako vidi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana.'
 
   return (
-    <div className={size === 'big' && 'w-[400px] h-[350px] flex' || size === 'middle' && 'w-[200px] h-[350px] overflow-hidden border-b border-black' || size === 'small' && 'w-[400px] h-[170px] flex'}>
+    <div className={size === 'big' && 'col-span-2 h-[350px] flex' || size === 'middle' && 'col-span-1 h-[350px] border-b border-black' || size === 'small' && 'col-span-1 h-[170px] flex'}>
+    {/* <div className={size === 'big' && 'w-[400px] h-[350px] flex' || size === 'middle' && 'w-[200px] h-[350px] overflow-hidden border-b border-black' || size === 'small' && 'w-[400px] h-[170px] flex'}> */}
 
       <ImageWrapper
-        source={require('../../public/sefik.png')}
+        source={news.image}
         width={size === 'big' && '50%' || size === 'middle' && '100%' || size === 'small' && '100%'}
-        height={size === 'small' && '100%'}
       />
 
-      {size === 'big' || size === 'middle' ? <div className={size === 'big' ? 'w-[50%] h-full flex flex-col justify-between border-t border-r border-b border-black p-3' : 'h-full flex flex-col gap-6 border-r border-l border-black p-3'}>
-        <h1 className='text-[16px] font-semibold'>Schmidt: BiH neće postati </h1>
-        <p className='text-[12px]'>{size === 'middle' ? desc.slice(0, 90)+'...' : desc}</p>
+      {size === 'big' || size === 'middle' ? <div className={size === 'big' ? 'w-[50%] h-full flex flex-col justify-between border-t border-r border-b border-black p-3' : 'h-[51.3%] flex flex-col justify-between border-r border-l border-black p-3'}>
+        <h1 className='text-[15px] font-semibold'>{news.title}</h1>
+        <p className='text-[12px]'>{size === 'middle' ? news.desc.slice(0, 90)+'...' : news.desc}</p>
         <div>
           <div className='bg-black w-[80%] h-[0.5px] mb-2' />
-          <p className='text-[12px]'>Author: Admin</p>
+          <p className='text-[12px]'>Author: {news.author}</p>
         </div>
       </div> : (
         <div className='w-[100%] h-full flex flex-col justify-center items-center border-t border-r border-b border-black p-3'>
-          <h1 className='text-[16px] font-semibold'>Schmidt: BiH neće postati </h1>
+          <h1 className='text-[16px] font-semibold'>{news.title}</h1>
         </div>
       )}
 
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,6 +6,8 @@ import SingleNews from '@/components/SingleNews'
 import Image from 'next/image'
 import React from 'react'
 
+const orderOfGrid = ['big', 'middle', 'middle', 'middle', 'middle', 'big', 'middle', 'middle', 'small', 'small']
+
 const data = [
   {
     id: 0,
@@ -30,7 +32,71 @@ const data = [
     author: 'Admin',
     image: require('../../public/sefik.png'),
     size: 'middle'
-  }
+  },
+  {
+    id: 3,
+    title: 'Schmidt: BiH neće postati',
+    desc: 'Visoki predstavnik u BiH danas je u Sarajevu održao nastupnu konferenciju za medije, na kojoj je govorio kako vidi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana.',
+    author: 'Admin',
+    image: require('../../public/sefik.png'),
+    size: 'middle'
+  },
+  {
+    id: 4,
+    title: 'Schmidt: BiH neće postati',
+    desc: 'Visoki predstavnik u BiH danas je u Sarajevu održao nastupnu konferenciju za medije, na kojoj je govorio kako vidi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana.',
+    author: 'Admin',
+    image: require('../../public/sefik.png'),
+    size: 'middle'
+  },
+  {
+    id: 5,
+    title: 'Schmidt: BiH neće postati',
+    desc: 'Visoki predstavnik u BiH danas je u Sarajevu održao nastupnu konferenciju za medije, na kojoj je govorio kako vidi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana.',
+    author: 'Admin',
+    image: require('../../public/sefik.png'),
+    size: 'big'
+  },
+  {
+    id: 5,
+    title: 'Schmidt: BiH neće postati',
+    desc: 'Visoki predstavnik u BiH danas je u Sarajevu održao nastupnu konferenciju za medije, na kojoj je govorio kako vidi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana.',
+    author: 'Admin',
+    image: require('../../public/sefik.png'),
+    size: 'middle'
+  },
+  {
+    id: 5,
+    title: 'Schmidt: BiH neće postati',
+    desc: 'Visoki predstavnik u BiH danas je u Sarajevu održao nastupnu konferenciju za medije, na kojoj je govorio kako vidi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana.',
+    author: 'Admin',
+    image: require('../../public/sefik.png'),
+    size: 'middle'
+  },
+  {
+    id: 5,
+    title: 'Schmidt: BiH neće postati',
+    desc: 'Visoki predstavnik u BiH danas je u Sarajevu održao nastupnu konferenciju za medije, na kojoj je govorio kako vidi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana.',
+    author: 'Admin',
+    image: require('../../public/sefik.png'),
+    size: 'big'
+  },
+  {
+    id: 5,
+    title: 'Schmidt: BiH neće postati',
+    desc: 'Visoki predstavnik u BiH danas je u Sarajevu održao nastupnu konferenciju za medije, na kojoj je govorio kako vidi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana.',
+    author: 'Admin',
+    image: require('../../public/sefik.png'),
+    size: 'middle'
+  },
+  {
+    id: 5,
+    title: 'Schmidt: BiH neće postati',
+    desc: 'Visoki predstavnik u BiH danas je u Sarajevu održao nastupnu konferenciju za medije, na kojoj je govorio kako vidi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana. idi BiH i šta će raditi na razvoju države za bolji život svih građana.',
+    author: 'Admin',
+    image: require('../../public/sefik.png'),
+    size: 'middle'
+  },
 ]
 
 const Main = () => {
@@ -41,15 +107,32 @@ const Main = () => {
       <Navbar />
       <div className='flex justify-around gap-5 p-5'>
         <div className='bg-grey-400 w-[200px] border border-black' />
-        <div className='flex gap-2'>
-          <SingleNews size='big' />
-          <SingleNews size='middle' />
-          <div className='flex flex-col justify-between'>
-            <SingleNews size='small' />
-            <SingleNews size='small' />
+        <article className='w-[70%] flex flex-col gap-2'>
+         {/*  <div className='flex gap-2'>
+            <SingleNews size='big' />
+            <SingleNews size='middle' />
+            <SingleNews size='middle' />
           </div>
-        </div>
-      </div>
+          <div className='flex gap-2'>
+            <SingleNews size='middle' />
+            <SingleNews size='middle' />
+            <SingleNews size='big' />
+          </div>
+          <div className='flex gap-2'>
+            <SingleNews size='middle' />
+            <SingleNews size='middle' />
+            <div className='flex flex-col justify-between'>
+              <SingleNews size='small' />
+              <SingleNews size='small' />
+            </div>
+          </div> */}
+          <div className='grid grid-cols-4 gap-2'>
+            {data.map(news => (
+              <SingleNews key={news.id} size={news.size} news={news} />
+            ))}
+          </div>
+      </article>
+    </div>
     </div>
   )
 }
