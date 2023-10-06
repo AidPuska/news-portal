b9ce1a00b09a44d5a869b1cef752a71ff8de8781
Aid Puska <aidpuska29@gmail.com>
2023-10-06 14:53:45 +0200

    resolved build errors

--- src/components/SingleNews.js
+++ src/components/SingleNews.js
@@ -14,7 +14,7 @@ const SingleNews = ({size, news}) => {
   return (
     <Link 
       href={{
-        pathname: `vijesti/${news.id}`,
+        pathname: `vijesti/${news?.id}`,
       }}
       className={size === 'big' && 'col-span-2 h-[350px] flex' || size === 'middle' && 'col-span-1 h-[350px] border-b border-black' || size === 'small' && 'col-span-1 h-[170px] flex'}
     >
@@ -22,21 +22,21 @@ const SingleNews = ({size, news}) => {
 
         <ImageWrapper
           addStyles='object-cover bg-red-500'
-          source={news.image}
+          source={news?.image}
           height={size === 'big' ? '100' : '50'}
           size={size}
         />
 
       {size === 'big' || size === 'middle' ? <div className={size === 'big' ? 'w-[50%] h-full flex flex-col justify-between border-t border-r border-b border-black/50 p-3 bg-[#E9ECEF]' : 'h-[50%] bg-[#E9ECEF] flex flex-col justify-between border-r border-l border-black p-3'}>
         <h1 className='text-[15px] font-semibold'>{news.title}</h1>
-        <p className='text-[12px]'>{size === 'middle' ? news.desc.slice(0, 90)+'...' : news.desc}</p>
+        <p className='text-[12px]'>{size === 'middle' ? news?.desc.slice(0, 90)+'...' : news.desc}</p>
         <div>
           <div className='bg-black w-[80%] h-[0.5px] mb-2' />
-          <p className='text-[12px]'>Author: {news.author}</p>
+          <p className='text-[12px]'>Author: {news?.author}</p>
         </div>
       </div> : (
         <div className='w-[100%] h-full flex flex-col justify-center items-center border-t border-r border-b border-black p-3'>
-          <h1 className='text-[16px] font-semibold'>{news.title}</h1>
+          <h1 className='text-[16px] font-semibold'>{news?.title}</h1>
         </div>
       )}
 
